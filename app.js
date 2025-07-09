05795f3764457e828af2001e03025ad96a907bea
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-07-09 11:29:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
+const port = 3000;
 const app = express();
 app.get('/', (req, res) => { 
   res.send('Hello, World!');
 })
-app.listen(3000, () => { });
+app.listen(port, () => { });
