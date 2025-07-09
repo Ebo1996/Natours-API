a1c54ef64202be8a23c8792af41b569bd5f97402
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-07-09 14:46:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'hello from server side', app: 'Natours' })
 })
 app.post('/', (req, res) => {
-  res.status(200).json({ message: 'hello from server side', app: 'Natours' })
+  res.send('you can post to this endpoint too');
 })
 app.listen(port, '127.0.0.1', () => { }); 
