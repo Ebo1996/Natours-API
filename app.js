ba38ac6319ff325bde255d35ae8140b287035fcf
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-07-09 14:23:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const port = 3000;
 const app = express();
 app.get('/', (req, res) => { 
-  res.send('Hello, World!');
+  res.status(200).json({ message: 'hello from server side', app: 'Natours' })
 })
-app.listen(port, '127.0.0.1', () => { });
+app.post('/', (req, res) => {
+  res.status(200).json({ message: 'hello from server side', app: 'Natours' })
+})
+app.listen(port, '127.0.0.1', () => { }); 
