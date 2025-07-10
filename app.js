eb46beed9b29d64b99e83ed019cc6f4311d6fca2
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-07-10 15:11:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const app = express();
 
 // app.get('/', (req, res) => {
 //   res.status(200).json({ message: 'hello from server side', app: 'Natours' })
-// })
+// });
 // app.post('/', (req, res) => {
 //   res.send('you can post to this endpoint too');
+// });
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-// })
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
