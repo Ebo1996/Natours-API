6a26604fed845867ec8843ddcb91010d9833f908
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-07-11 21:10:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,10 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
+  // 
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
   res.send('done')
 });
 app.listen(port, '127.0.0.1', () => {
