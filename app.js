d8923d5b1e90b0a0c7fc3efadc15aad47464255d
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-07-12 11:22:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,30 +13,42 @@ app.use(express.json());
 //   res.send('you can post to this endpoint too');
 // });
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: {
-      tours
-    }
+      tours,
+    },
   });
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  // 
+  //
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour
-      }
-    });
-  });
+
+  /*(path, data, callback)
+  ✔ JSON.stringify(tours) ➔ becomes a JSON string.
+
+✔ fs.writeFile(...) ➔ writes that string into the file on disk at the given path.*/
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(port, '127.0.0.1', () => {
