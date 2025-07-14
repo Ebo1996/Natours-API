d5d6d01e0131a6c8f2e122a2b82943c3d82f4aca
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-07-14 10:01:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,17 +6,7 @@ const port = 3000;
 const app = express();
 
 app.use(express.json());
-// app.get('/', (req, res) => {
-//   res.status(200).json({ message: 'hello from server side', app: 'Natours' })
-// });
-// app.post('/', (req, res) => {
-//   res.send('you can post to this endpoint too');
-// });
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -24,7 +14,12 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
-});
+}
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours');
 
 app.get('/api/v1/tours/:id', (req, res) => {
   console.log(req.params);
