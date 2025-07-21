47e60b56501797701d61d1db7e6df1275a13415a
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-07-21 15:40:16 +0300

    Update tourcontroller.js

--- controllers/tourcontroller.js
+++ controllers/tourcontroller.js
@@ -1,6 +1,12 @@
-fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+const fs = require('fs');
+
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
-getAllTours = (req, res) => {
+
+
+exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
 
   res.status(200).json({
@@ -8,13 +14,12 @@ getAllTours = (req, res) => {
     requestedAt: req.requestTime,
     results: tours.length,
     data: {
-      tours
-    }
+      tours,
+    },
   });
 };
 
-const getTour = (req, res) => {
-  console.log(req.params);
+exports.getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
 
@@ -33,14 +38,13 @@ const getTour = (req, res) => {
   });
 };
 
-// Create a tour
-const createTour = (req, res) => {
+exports.createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
 
   fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
+    `${__dirname}/../dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
       res.status(201).json({
@@ -53,8 +57,7 @@ const createTour = (req, res) => {
   );
 };
 
-// Delete a tour
-const deleteTour = (req, res) => {
+exports.deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -68,8 +71,7 @@ const deleteTour = (req, res) => {
   });
 };
 
-// Update a tour
-const updateTour = (req, res) => {
+exports.updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -77,8 +79,10 @@ const updateTour = (req, res) => {
     });
   }
 
-  res.status(204).json({
+  res.status(200).json({
     status: 'success',
-    data: null,
+    data: {
+      message: 'Tour updated successfully (mock)',
+    },
   });
 };
