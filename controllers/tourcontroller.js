b0108ebe0d1d96b144d1af711800b7d3f2d426c3
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-07-22 10:50:58 +0300

    Update tourcontroller.js

--- controllers/tourcontroller.js
+++ controllers/tourcontroller.js
@@ -5,6 +5,13 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  if (val * 1 > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+  next();
+};
+
 
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -23,12 +30,6 @@ exports.getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
 
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
 
   res.status(200).json({
     status: 'success',
