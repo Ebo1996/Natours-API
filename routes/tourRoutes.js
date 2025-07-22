4a14a24d4aff3788f1d59b9f61e398b630a21212
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-07-22 10:05:08 +0300

    Update tourRoutes.js

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const tourController = require('./../controllers/tourcontroller');
 const router = express.Router();
+router.param('id', (req, res, next, val) => {
+  console.log(`Tour id is ${val}`);
+  next();
+});
+
 router
   .route('/')
   .get(tourController.getAllTours)
