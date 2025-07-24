fc895d586076c15b8bc7a002f5349b0073ceb30d
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-07-24 09:40:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 console.log(process.env);
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, '127.0.0.1', () => {
   console.log(`Server running on http://127.0.0.1:${port}`);
 });
