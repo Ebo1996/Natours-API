ca2d5a9a943d7e21c1222f8c0a1afbae3f7dd13a
Ebo1996 <ebisaberhanu1996@gmail.com>
2025-07-24 09:33:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+const dotenv = require('dotenv');
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
 console.log(process.env);
 const port = 3000;
 app.listen(port, '127.0.0.1', () => {
